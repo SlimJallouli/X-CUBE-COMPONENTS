66fc0b71fb5310a6b366e9d900de2dd259a406f2
Slim JALLOULI <slim.jallouli@st.com>
2021-05-28 08:09:29 -0700

    bug fixes in app.c

--- Sources/Examples/Projects/Sensors/Environmental/HTS221/Inc/app.h
+++ Sources/Examples/Projects/Sensors/Environmental/HTS221/Inc/app.h
@@ -1,5 +1,23 @@
-#include "main.h"
-#include <stdio.h>
+/*
+  ******************************************************************************
+  * @file           : app.h
+  * @brief          : Main application header
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
+#ifndef _APP_H_
+#define _APP_H_
 
 #define APP_DEBUG
 
@@ -11,3 +29,5 @@
 
 int App_Init(void);
 int App_Run(void);
+
+#endif /* _APP_H_ */
--- Sources/Examples/Projects/Sensors/Environmental/HTS221/Src/app.c
+++ Sources/Examples/Projects/Sensors/Environmental/HTS221/Src/app.c
@@ -1,20 +1,28 @@
+/*
+  ******************************************************************************
+  * @file           : app.c
+  * @brief          : Main application
+  ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 #include "main.h"
 
 #include "app.h"
-#include "temperature_sensor.h"
-#include "humidity_sensor.h"
+#include "hts221.h"
 #include <stdio.h>
 
-
-TEMPERATURE_SENSOR_Object_t TMP_Obj;
-uint8_t TMP_Id;
-float   TMP_Odr;
-uint8_t TMP_Status;
-
-HUMIDITY_SENSOR_Object_t   HUM_Obj;
-uint8_t HUM_Id;
-float   HUM_Odr;
-uint8_t HUM_Status;
+HTS221_Object_t HTS221_Obj;
+uint8_t HTS221_id;
 
 /**
 * @brief  Send HTML page
@@ -25,21 +33,17 @@ int App_Init(void)
 {
 uint32_t err = 0;
 
-  TMP_Obj.is_initialized  = 0;
-  TMP_Obj.hum_is_enabled  = 0;
-  TMP_Obj.temp_is_enabled = 0;  
+  HTS221_Obj.IO.BusType      =  HTS221_I2C_BUS;
+  HTS221_Obj.IO.Address      =  HTS221_I2C_ADDRESS;
+  HTS221_Obj.IO.hi2c         = &HTS221_I2C_HANDLER; 
+  HTS221_Obj.is_initialized  = 0;
+  HTS221_Obj.hum_is_enabled  = 0;
+  HTS221_Obj.temp_is_enabled = 0;  
 
-  err = TEMPERATURE_SENSOR_Init  (&TMP_Obj);
-  err = TEMPERATURE_SENSOR_Enable(&TMP_Obj);
-  err = TEMPERATURE_SENSOR_ReadID(&TMP_Obj, &TMP_Id);
-
-  HUM_Obj.is_initialized  = 0;
-  HUM_Obj.hum_is_enabled  = 0;
-  HUM_Obj.temp_is_enabled = 0;  
-
-  err = HUMIDITY_SENSOR_Init  (&HUM_Obj);
-  err = HUMIDITY_SENSOR_Enable(&HUM_Obj);
-  err = HUMIDITY_SENSOR_ReadID(&HUM_Obj, &HUM_Id);
+  err = HTS221_ReadID     (&HTS221_Obj, &HTS221_id);
+  err = HTS221_Init       (&HTS221_Obj);
+  err = HTS221_TEMP_Enable(&HTS221_Obj);
+  err = HTS221_HUM_Enable (&HTS221_Obj);
 
   if(err)
   {
@@ -55,8 +59,8 @@ int App_Run(void)
   float    fTemp;
   float    fHum;
   
-  err = TEMPERATURE_SENSOR_GetTemperature(&TMP_Obj, &fTemp); 
-  err = HUMIDITY_SENSOR_GetHumidity      (&HUM_Obj, &fHum);
+  err = HTS221_TEMP_GetTemperature(&HTS221_Obj, &fTemp); 
+  err = HTS221_HUM_GetHumidity    (&HTS221_Obj, &fHum);
   
   if(err)
   {
